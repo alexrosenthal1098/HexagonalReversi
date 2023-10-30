dba96fc8b88cc63cd451b99a3354b7eac4049de7
Alex Rosenthal <alexrosen445@gmail.com>
2023-10-30 16:29:12 -0400

    Update ReversiPlayer.java

--- src/Player/ReversiPlayer.java
+++ src/Player/ReversiPlayer.java
@@ -12,7 +12,7 @@ public interface ReversiPlayer {
   /**
    * Returns the coordinates of the tile that this player has chosen to make the next
    * move at on the given model.
-   * @param model The model that the player uses to choose the next move.
+   * @param model The model (read-only) that the player uses to choose the next move.
    * @return A Point representing the location of the tile to play at.
    */
   Point getNextMove(ViewReversiModel model);
